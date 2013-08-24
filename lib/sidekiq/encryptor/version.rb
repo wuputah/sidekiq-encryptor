3da6065bd4fd49ff65a903fd860f69c84f3ef995
Jonathan Dance <jd@wuputah.com>
2013-08-23 17:31:32 -0700

    0.1.0pre

--- lib/sidekiq/encryptor/version.rb
+++ lib/sidekiq/encryptor/version.rb
@@ -3,9 +3,9 @@ module Sidekiq
 
     module Version
       MAJOR = 0
-      MINOR = 0
-      PATCH = 1
-      SUFFIX = ""
+      MINOR = 1
+      PATCH = 0
+      SUFFIX = "pre"
     end
 
     VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::PATCH}#{Version::SUFFIX}"
